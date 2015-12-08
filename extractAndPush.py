6f4a85f80ba402e5f8ea69eff14bc941bbfa704d
Varun Ved <varunmved@gmail.com>
2015-12-07 20:44:36 -0800

    fixes

--- extractAndPush.py
+++ extractAndPush.py
@@ -3,8 +3,9 @@ import os
 import sys
 import urlparse
 
+DATABASE_URL = "postgres://odtcggsjshuzqc:_dcb6-RrKLBvXKVAZ-J38NiSoz@ec2-54-204-5-56.compute-1.amazonaws.com:5432/d95i58bvuk7cle"
 urlparse.uses_netloc.append("postgres")
-url = urlparse.urlparse(os.environ["DATABASE_URL"])
+url = urlparse.urlparse(os.environ[DATABASE_URL])
 
 conn = psycopg2.connect(
         database = url.pth[1:],
