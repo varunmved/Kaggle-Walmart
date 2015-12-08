7a04886f6bfad0dae6ceba642ea5d423c4004aff
Varun Ved <varunmved@gmail.com>
2015-12-07 18:18:01 -0800

    adding web routes

--- extract/extractAndPush.py
+++ extract/extractAndPush.py
@@ -1,6 +1,18 @@
 import psycopg2
+import os
+import sys
+import urlparse
 
-def load():
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
+conn = psycopg2.connect(
+        database = url.pth[1:],
+        user = url.username,
+        password = url.password,
+        host = url.hostname,
+        port = url.port
+)
 
 
 
