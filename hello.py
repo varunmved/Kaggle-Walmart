06497f12e82b7e7d1adca5f26069a7e77c7754b8
Varun Ved <varunmved@gmail.com>
2015-12-08 18:31:50 -0800

    pushing

--- hello.py
+++ hello.py
@@ -1,7 +1,10 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, request
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_UI'] = 'postgresql://localhost/testDB'
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello():
